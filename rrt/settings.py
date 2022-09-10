c3a2ae6da1d0e2343b6683d2422ccfcf56dc550c
anand-kulkarni25 <anandkulkarni213@gmail.com>
2022-09-10 17:17:19 +0530

    vercel try 1

--- rrt/settings.py
+++ rrt/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'rrt.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'reading',
+        'USER': 'postgres',
+        'PASSWORD': 'Lenovo@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
