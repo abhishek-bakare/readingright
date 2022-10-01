b1ca305b450e029ee5aa69684a611da0fc489fcf
anand-kulkarni25 <anandkulkarni213@gmail.com>
2022-10-01 22:05:39 +0530

    nc

--- rrt/settings.py
+++ rrt/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'rrt.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'reading',
-        'USER': 'postgres',
-        'PASSWORD': 'Lenovo@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
     #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'reading',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'Lenovo@123',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432',
     #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
